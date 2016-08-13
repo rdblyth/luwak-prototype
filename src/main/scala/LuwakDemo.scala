db0613ea2688e2202c7394293bdbd3899da0f360
Daneil Blyth <rdblyth@gmail.com>
2016-08-13 18:39:59 -0400

    Updated to handle multiple query files and documents with gzip format

--- src/main/scala/LuwakDemo.scala
+++ src/main/scala/LuwakDemo.scala
@@ -1,50 +1,59 @@
-import java.io.File
+import java.io.{File, FileInputStream}
+import java.util.zip.GZIPInputStream
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.slf4j.LoggerFactory
 import uk.co.flax.luwak.matchers.SimpleMatcher
 import uk.co.flax.luwak._
-import uk.co.flax.luwak.presearcher.TermFilteredPresearcher
+import uk.co.flax.luwak.presearcher.{MatchAllPresearcher, TermFilteredPresearcher}
 import uk.co.flax.luwak.queryparsers.LuceneQueryParser
 
 import scala.collection.JavaConversions._
 import scala.io.{Codec, Source}
 
 object LuwakDemo extends App {
-  val queriesFile = "src/main/resources/queries.txt"
-  val documentsDir = "src/main/resources/gutenberg"
+  val queriesDir = "src/main/resources/queries"
+  val documentsDir = "src/main/resources/docs"
   val FIELD = "text"
 
   val logger = LoggerFactory.getLogger("LuwakDemo")
   val analyzer = new StandardAnalyzer
 
-  val monitor = new Monitor(new LuceneQueryParser(FIELD, analyzer), new TermFilteredPresearcher())
-  monitor.update(getQueries(queriesFile))
+  //val monitor = new Monitor(new LuceneQueryParser(FIELD, analyzer), new TermFilteredPresearcher)
+  val monitor = new Monitor(new LuceneQueryParser(FIELD, analyzer), new MatchAllPresearcher)
+  monitor.update(getQueries(queriesDir).toList)
   logger.info(s"Added ${monitor.getQueryCount} queries to monitor")
 
-  val documents = buildDocs(documentsDir).toList
-  logger.info(s"Loaded ${documents.size} documents")
-
-  val docMatches = documents.map(doc => (doc, monitor.`match`(doc, SimpleMatcher.FACTORY)))
-  docMatches.foreach{ case(doc, matches) => logDocumentMatches(doc, matches) }
-
+  val docMatches = matchDocuments(documentsDir, monitor)
   val totalTime = docMatches.map(_._2.getSearchTime()).sum
-  logger.info(s"Total time to match all documents: $totalTime ms")
+  logger.info(s"Matched ${docMatches.size} documents in $totalTime ms")
+  logger.info(s"Average time to process a document: ${totalTime / docMatches.size} ms")
 
-  def getQueries(queriesFile: String) = {
-    logger.info(s"Loading queries from $queriesFile")
-    val lines = Source.fromFile(queriesFile).getLines
-    lines.zipWithIndex.map { case (query, count) => new MonitorQuery(s"$count-$query", query) }.toList
-  }
+  monitor.close()
 
-  def buildDocs(directory: String) = {
+  def getQueries(directory: String) = {
+    logger.info(s"Loading queries from $queriesDir")
     for (file <- new File(directory).listFiles()) yield {
-      val content = Source.fromFile(file)(Codec.ISO8859).getLines.mkString("\n")
-      InputDocument.builder(file.getName).addField(FIELD, content, new StandardAnalyzer).build()
+      val query = Source.fromFile(file)(Codec.ISO8859).getLines.mkString("\n")
+      new MonitorQuery(file.getName, query)
     }
   }
 
-  def logDocumentMatches(doc: InputDocument, matches: Matches[QueryMatch]) = {
-    logger.info(s"Documents ${doc.getId} was matched in ${matches.getSearchTime()} ms with ${matches.getQueriesRun()} queries run")
+  def matchDocuments(documentDir: String, monitor: Monitor) = {
+    val documentFiles = new File(documentsDir).listFiles()
+    for(file <- documentFiles) yield {
+      val matches = monitor.`match`(buildDocument(file), SimpleMatcher.FACTORY)
+      logDocumentMatches(file.getName, matches)
+      (file.getName, matches)
+    }
+  }
+
+  def buildDocument(file: File) = {
+    val content = Source.fromInputStream(new GZIPInputStream(new FileInputStream(file))).getLines().mkString("\n")
+    InputDocument.builder(file.getName).addField(FIELD, content, new StandardAnalyzer).build()
+  }
+
+  def logDocumentMatches(docName: String, matches: Matches[QueryMatch]) = {
+    logger.info(s"Documents $docName was matched in ${matches.getSearchTime()} ms with ${matches.getQueriesRun()} queries run")
   }
 }
